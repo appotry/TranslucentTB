94db5f4bf160433318a1d912cf381daea5d32f34
Charles Milette <charles.milette@gmail.com>
2024-05-29 20:59:23 -0400

    Migrate to Trusted Signing

--- .azp/scripts/sign-msix.ps1
+++ .azp/scripts/sign-msix.ps1
@@ -1,7 +1,7 @@
 param(
     [string] $Platform,
 	[string] $Configuration,
-	[string] $AcsMetadata,
+	[string] $TrustedSigningMetadata,
 	[string] $DigestAlgorithm,
 	[string] $TimestampServer
 )
@@ -13,10 +13,10 @@ $msixPackage = (Get-ChildItem "$msixPackageFolder" -Filter "*.msix").FullName
 
 [xml]$config = Get-Content "$PSScriptRoot\..\..\TranslucentTB\packages.config"
 
-$acsClientVersion = ($config.packages.package | Where { $_.id -eq "Azure.CodeSigning.Client" }).version
+$trustedSigningClientVersion = ($config.packages.package | Where { $_.id -eq "Microsoft.Trusted.Signing.Client" }).version
 $sdkVersion = ($config.packages.package | Where { $_.id -eq "Microsoft.Windows.SDK.BuildTools" }).version
 
-$acsClientFolder = "$PSScriptRoot\..\..\packages\Azure.CodeSigning.Client.$acsClientVersion\bin"
+$trustedSigningClientFolder = "$PSScriptRoot\..\..\packages\Microsoft.Trusted.Signing.Client.$trustedSigningClientVersion\bin"
 $sdkFolder = (Get-ChildItem "$PSScriptRoot\..\..\packages\Microsoft.Windows.SDK.BuildTools.$sdkVersion\bin").FullName
 
-&"$sdkFolder\x86\signtool.exe" sign /ph /tr "$TimestampServer" /td $DigestAlgorithm /fd $DigestAlgorithm /dlib "$acsClientFolder\x86\Azure.CodeSigning.Dlib.dll" /dmdf "$AcsMetadata" "$msixPackage"
+&"$sdkFolder\x64\signtool.exe" sign /ph /tr "$TimestampServer" /td $DigestAlgorithm /fd $DigestAlgorithm /dlib "$trustedSigningClientFolder\x64\Azure.CodeSigning.Dlib.dll" /dmdf "$TrustedSigningMetadata" "$msixPackage"
